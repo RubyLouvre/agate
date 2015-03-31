e1ec6a15942bf04e2f565bfe5985674bfb254323
RubyLouvre <cheng19840218@gmail.com>
2015-03-31 20:32:19 +0800

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var koa = require('koa');
 var render = require('koa-ejs');
 var path = require('path');
 var app = koa();
+var http = require("http")
 var router = require('koa-router')();
 
 
@@ -43,11 +44,57 @@ router
      this.body = "这是用户列表3"
   })
   app.on("error", function (e){
+      //https://github.com/koajs/examples/issues/20
         log.error('server error', err, ctx);
   })
 app.use(router.routes())
 app.use(router.allowedMethods());
+//错误处理
+//https://github.com/koajs/onerror/blob/master/index.js
+function * errors (next) {
+  try {
+    yield next;
+  } catch (err) {
+      console.error("err.stack");
+    
+    // 取得行号
+  	var line = err.stack.split('\n')[1];
 
+  	// parse file path and line
+  	var result = /at\s(.+\s)?\(?(.+)\:([0-9]+)\:[0-9]+/.exec(line);
+  	var path = result[2];
+  	var row = +result[3];
+
+      // delegate
+    this.app.emit('error', err, this);
+
+    // nothing we can do here other
+    // than delegate to the app-level
+    // handler and log.
+    if (this.headerSent || !this.writable) {
+      err.headerSent = true;
+      return;
+    }
+
+    // ENOENT support
+    if ('ENOENT' === err.code) {
+      err.status = 404;
+    }
+
+    if ('number' !== typeof err.status || !http.STATUS_CODES[err.status]) {
+      err.status = 500;
+    }
+    this.status = err.status;
+
+    yield this.render('error', {
+  		name: err.name,
+  		message: err.message,
+  		line: row,
+  		stack: err.stack
+  	});
+  }
+ }
+  app.use(errors)
 // catch all middleware, only land here
 // if no other routing rules match
 // make sure it is added after everything else
