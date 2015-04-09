8a96c796e397c3465db0859d4c8cde663cf28d25
ilife5 <ilife5@qq.com>
2015-04-09 22:11:09 +0800

    format

--- config/fekitVersion.js
+++ config/fekitVersion.js
@@ -1,30 +1,30 @@
-var fs = require('fs');
+var fs = require('fs') 
 
-var VERSION_FILE; //version.mapping的文件路径
-var QZZ_PATH; //qzz项目的路径
-var VERS; //版本号的map
+var VERSION_FILE  //version.mapping的文件路径
+var QZZ_PATH  //qzz项目的路径
+var VERS  //版本号的map
 
 function getVersion() {
 
-    var mapping, arr, ver = {};
-    mapping = fs.readFileSync(VERSION_FILE, 'utf-8');
-    arr = mapping.split('\n');
+    var mapping, arr, ver = {}
+    mapping = fs.readFileSync(VERSION_FILE, 'utf-8')
+    arr = mapping.split('\n')
     arr.forEach(function (val) {
-        var kv = val.split('#');
-        ver[kv[0].replace(/^\.[\/\\]/, '')] = kv[1];
-    });
-    return ver;
+        var kv = val.split('#') 
+        ver[kv[0].replace(/^\.[\/\\]/, '')] = kv[1] 
+    }) 
+    return ver 
 
 }
 
 exports.getVersion = function () {
-    return VERS;
-};
+    return VERS 
+} 
 
 
 exports.flushVersions = function () {
-    VERS = getVersion();
-    exports.vers = VERS;
+    VERS = getVersion() 
+    exports.vers = VERS 
 }
 
 /**
@@ -32,23 +32,23 @@ exports.flushVersions = function () {
  * @param opts
  */
 exports.init = function (opts) {
-    VERSION_FILE = opts.path;
-    QZZ_PATH = [opts.qzzHost , '/', opts.qzz , '/prd/'].join('');
-    exports.flushVersions();
+    VERSION_FILE = opts.path 
+    QZZ_PATH = [opts.qzzHost , '/', opts.qzz , '/prd/'].join('') 
+    exports.flushVersions() 
 }
 
 exports.version = function version(key) {
 
-    key = key.replace(/^[\.\/\\]+/, '');
-    var ver;
+    key = key.replace(/^[\.\/\\]+/, '') 
+    var ver 
 
     if (key in VERS) {
-        ver = VERS[key] || '';
+        ver = VERS[key] || '' 
     } else {
-        ver = 'dev';
+        ver = 'dev' 
     }
     key = key.replace(/(\.js|\.css)$/, '@' + ver + '$1')
 
-    return QZZ_PATH + key;
+    return QZZ_PATH + key 
 
-};
+} 
