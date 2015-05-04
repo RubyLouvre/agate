3e86a7872e694b4c6e2381c893957a4c24d9063e
RubyLouvre <cheng19840218@gmail.com>
2015-05-04 17:52:58 +0800

    update

---
+++ app/pages/bigpipe/controller.js
@@ -0,0 +1,16 @@
+var path = require("path")
+exports.index = function * (next) {
+    var BigPipe = require("bigpipe")
+    var b = new BigPipe(4)
+    var layout = app.getLayout("template-bigpipe.html")
+    this.body = b.stream
+    this.type = "html"
+    b.begin(layout)
+    b.flush(path.join(__dirname, "./index.html"), 0)
+    b.flush(path.join(__dirname, "./left.html"), 1)
+    setTimeout(function () {
+        b.flush(path.join(__dirname, "./middle.html"), 2)
+    }, 1000)
+    b.flush(path.join(__dirname, "./right.html"), 3)
+//https://www.ibm.com/developerworks/cn/java/j-lo-bigpipe/
+}
