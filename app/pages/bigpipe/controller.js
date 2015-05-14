177550ef3a076a7aea19d898e922c6b717cb6f9d
RubyLouvre <cheng19840218@gmail.com>
2015-05-14 14:49:31 +0800

    update bigpipe

--- app/pages/bigpipe/controller.js
+++ app/pages/bigpipe/controller.js
@@ -5,21 +5,14 @@ exports.index = function * (next) {
     var layout = app.getLayout("template-bigpipe.html")
     this.body = b.stream
     this.type = "html"
- 
+
     b.begin(layout)
-    
-    var end = BigPipe.heredoc(function(){
-        /*
-      <%scripts.forEach(function(script) {%>
-                 <script type="text/javascript" src="<%=: script|qzzUrl %>"></script>
-             <%})%>
-         </body>
-       </html>
-         */
-    })
-    b.end(end)
+
     b.flush(path.join(__dirname, "./index.html"), 0)
-    b.flush(path.join(__dirname, "./left.html"), 1)
+
+    setTimeout(function () {
+        b.flush(path.join(__dirname, "./left.html"), 1)
+    }, 2000)
     setTimeout(function () {
         b.flush(path.join(__dirname, "./middle.html"), 2)
     }, 1000)
